4179ece05f7e639924466a0f6e82af54ec48c043
Kimberly Winter <kwinter@olin.edu>
2019-02-11 02:18:11 -0500

    Update createFileforTransmit.m

--- createFileforTransmit.m
+++ createFileforTransmit.m
@@ -1,4 +1,4 @@
-function message2send=createFileforTransmit(header, footer, message, imagmessage)
+function message2send=createFileforTransmit(header, footer, message)
 
 %tx_samples_from_file ??freq 2478e6 ??rate 2e5 ??type float ??ant “TX/RX” ??subdev ”A:A” ??gain 40 ??file
 %tx.dat
@@ -10,19 +10,19 @@ function message2send=createFileforTransmit(header, footer, message, imagmessage
     
     %adding header to beginning
     for i=1:length(header)
-       message2send(i)=header(i)+header(i)*j;
+       message2send(i)=header(i);
     end
     
     %adding message, where every bit is repeated 1000 times
     for i=1:length(message)
         for k=1:1000
-            message2send(length(header)+((i-1)*1000)+k)= message(i) +imagmessage(i)*j;
+            message2send(length(header)+((i-1)*1000)+k)= message(i);
         end
     end
     
     %adding footer to end
     for i=1:length(footer)
-       message2send(length(header)+length(message)*1000+i)=footer(i) + footer(i)*j;
+       message2send(length(header)+length(message)*1000+i)=footer(i);
     end
     
     write_usrp_data_file(message2send);
